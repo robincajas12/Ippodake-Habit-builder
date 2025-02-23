571a516defa9ecc5b9d9b841f7e4f55e269d4a4d
robincajas12 <robincajas2020@gmail.com>
2025-02-23 08:39:41 -0500

    delete source/source forlder i don't know why it was there

--- source/source/Views/Components/Styles/stylesMainContentView.js
+++
@@ -1,7 +0,0 @@
-import { StyleSheet } from "react-native";
-import _vw from "../../../utils/sizeConversors";
-
-const stylesMainContentView = StyleSheet.create({
-    view:{flex: 8, display:'flex',  alignContent:'center'}
-});
-export default stylesMainContentView;
